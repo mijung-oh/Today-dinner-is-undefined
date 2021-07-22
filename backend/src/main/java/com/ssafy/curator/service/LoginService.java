129a7341d4839fb789a20b8792848552b23f8345
mijung-oh <ks03102@naver.com>
2021-07-22 11:14:31 +0900

    remove backend

--- backend/src/main/java/com/ssafy/curator/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.curator.service;
-
-import com.ssafy.curator.dto.user.UserSessionDto;
-
-public interface LoginService {
-    UserSessionDto getSession();
-    void setSession(UserSessionDto sessionDto);
-    String getSessionId();
-}
--- backend/src/main/java/com/ssafy/curator/vo/RequestUser.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.curator.vo;
-
-import lombok.Data;
-
-@Data
-public class RequestUser {
-    private String email;
-    private String name;
-}
