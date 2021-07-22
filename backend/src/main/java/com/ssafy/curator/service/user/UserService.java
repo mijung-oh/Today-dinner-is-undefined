55bdd4a69c279602f4b376885ae3ea5ad8d48b3e
mijung-oh <ks03102@naver.com>
2021-07-22 11:15:30 +0900

    remove backend

--- backend/src/main/java/com/ssafy/curator/service/user/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.curator.service.user;
-
-import com.ssafy.curator.dto.user.UserDto;
-
-public interface UserService {
-    UserDto getUserByUserEmail(String email);
-    void createPlatformUser(UserDto userDto, String platform);
-    boolean existUser(String email);
-}
