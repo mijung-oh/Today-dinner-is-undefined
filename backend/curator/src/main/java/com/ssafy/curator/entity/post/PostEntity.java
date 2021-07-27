aae1dee18f5bea1dd3417b439d5cb354877c1f16
류지우 <yo94lover@naver.com>
2021-07-27 15:32:45 +0900

    Update getAllPost details

--- backend/curator/src/main/java/com/ssafy/curator/entity/post/PostEntity.java
+++ backend/curator/src/main/java/com/ssafy/curator/entity/post/PostEntity.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -26,8 +27,13 @@ public class PostEntity {
     @JoinColumn(name = "userId")
     UserEntity user;
 
+    @NotNull
     String title;
+
+    @NotNull
     String description;
+
+    @NotNull
     String ingredients;
 
     @CreationTimestamp
