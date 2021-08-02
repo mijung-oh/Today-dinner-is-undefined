aad283522a0dc4008d848883dc4d564570c8c07a
SUHYEOK97 <rlatngur10@naver.com>
2021-08-02 14:01:59 +0900

    배포 환경 구성

--- backend/curator/src/main/java/com/ssafy/curator/config/WebConfig.java
+++ backend/curator/src/main/java/com/ssafy/curator/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.curator.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,8 +12,13 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://127.0.0.1:8080", "http://localhost:8080", "http://127.0.0.1:9000", "http://localhost:9000", "http://127.0.0.1:3000", "http://localhost:3000")
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name())
                 .exposedHeaders("*")
                 .allowCredentials(true);
-
     }
 }
