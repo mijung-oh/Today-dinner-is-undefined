7562afb9ab887a8cd882a3f4b1d449dc3cd4da5b
SUHYEOK97 <rlatngur10@naver.com>
2021-08-03 18:34:36 +0900

    webConfig 수정

--- backend/curator/src/main/java/com/ssafy/curator/config/WebConfig.java
+++ backend/curator/src/main/java/com/ssafy/curator/config/WebConfig.java
@@ -11,10 +11,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:9000", "http://localhost:9000", "http://127.0.0.1:3000", "http://localhost:3000",
-                                "http://i5c207.p.ssafy.io",".i5c207.p.ssafy.io",
-                                "http://i5c207.p.ssafy.io:9000","http://i5c207.p.ssafy.io:9000/",
-                                "http://i5c207.p.ssafy.io:3000","http://i5c207.p.ssafy.io:3000/")
+                .allowedOrigins("http://*","https://*")
+                .allowedOriginPatterns("http://*")
+//                .allowedOrigins("http://127.0.0.1:9000", "http://localhost:9000", "http://127.0.0.1:3000", "http://localhost:3000",
+//                                "http://i5c207.p.ssafy.io",".i5c207.p.ssafy.io",
+//                                "http://i5c207.p.ssafy.io:9000","http://i5c207.p.ssafy.io:9000/",
+//                                "http://i5c207.p.ssafy.io:3000","http://i5c207.p.ssafy.io:3000/")
                 .allowedMethods(
                         HttpMethod.GET.name(),
                         HttpMethod.HEAD.name(),
