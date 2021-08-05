eccc2b7a4f4d4f32364ad00a727b1988c9fd971b
lwj-awesome <lwj1267@naver.com>
2021-08-05 15:21:44 +0900

    articlepage_modified

--- frontend/curator/src/Articles/page/DetailPage.jsx
+++ frontend/curator/src/Articles/page/DetailPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import CommentCreate from "./CommentCreate";
+import CommentList from "./CommentList";
 function DetailPage({ article, onDelete }) {
   /*
   props article 안에 백 서버에 있는 필드 모든 값이 담겨 있음
@@ -13,13 +15,24 @@ function DetailPage({ article, onDelete }) {
           <img src={img} />
         </div>
       ))}
-
+      <Link to="/articles">목록으로</Link>
       <h3>제목: {article.title}</h3>
       <h5>설명: {article.description}</h5>
       <h5>재료: {article.ingredients}</h5>
-      <h5>작성자: {article.user}</h5>
-      <h5>작성시간: {article.createDate}</h5>
-      <Link to={`/articles/update/${article.id}`}>
+
+      <h5>작성시간:{article.createDate}</h5>
+      <CommentList post_id={article.id} />
+      <CommentCreate post_id={article.id} />
+      <Link
+        to={{
+          pathname: `/articles/update/${article.id}`,
+          state: {
+            title: article.title,
+            description: article.description,
+            ingredients: article.ingredients,
+          },
+        }}
+      >
         <button>수정</button>
       </Link>
       <br />
