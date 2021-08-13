706ca5e63c903cfad8fc8f4bb88172959cfb1ecc
SpookyJelly <74237436+SpookyJelly@users.noreply.github.com>
2021-08-13 19:09:26 +0900

    midtem commit : editing modals

--- frontend/curator/src/pages/RecommandPage.tsx
+++ frontend/curator/src/pages/RecommandPage.tsx
@@ -1,5 +1,7 @@
-import React, { MouseEvent } from "react";
-// import Paper from "@material-ui/core/Paper";
+import React, { MouseEvent, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import axios from "axios";
+import { RECOMMEND_LIST_URL } from "@lib/constants";
 import { makeStyles, Theme } from "@material-ui/core/styles";
 import Chip from "@material-ui/core/Chip";
 import acorn from "@recommand/static/image/acorn.svg";
@@ -28,6 +30,10 @@ import wheat from "@recommand/static/image/wheat.svg";
 import { Paper, Avatar, Button } from "@material-ui/core";
 import Checkbox from "@material-ui/core/Checkbox";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { Ingredient } from "@lib/interfaces";
+import RecommandModal from "@recommand/components/RecommandModal";
+import Swal from "sweetalert2";
+import { useHistory } from "react-router";
 
 const imageObject = {
   acorn,
@@ -74,6 +80,7 @@ const useStyles = makeStyles((theme: Theme) => ({
     textAlign: "left",
     padding: "5px 10px",
     borderRadius: "40px",
+    backgroundColor: "#BADA55",
   },
   foodFieldset: {
     border: `solid thin ${theme.palette.grey[400]}`,
@@ -81,34 +88,6 @@ const useStyles = makeStyles((theme: Theme) => ({
   },
 }));
 
-interface Ingredient {
-  name:
-    | "acorn"
-    | "beef"
-    | "carrot"
-    | "chickenBreast"
-    | "chicken"
-    | "chives"
-    | "eggs"
-    | "greenOnion"
-    | "hairtail"
-    | "jelly"
-    | "mackerel"
-    | "noodles"
-    | "nori"
-    | "octopus"
-    | "paprika"
-    | "pork"
-    | "potato"
-    | "pumpkin"
-    | "ramen"
-    | "riceCake"
-    | "sesameLeaf"
-    | "vegetables"
-    | "wheat";
-  korean: string;
-  alt?: string; // 헉 대박 .. 콜론 앞에 ? 붙이면 | undefined와 같다
-}
 const mainIngredientMappingList: Ingredient[] = [
   {
     name: "beef",
@@ -229,31 +208,103 @@ const subIngredientMappingList: Ingredient[] = [
   },
 ];
 
-const testF = (event: React.ChangeEvent<HTMLInputElement>) => {
-  console.log("각종 양념장 테스트");
-};
-
-const testM = (event: MouseEvent) => {
-  event.stopPropagation();
-  console.log("test 1");
-  console.log(event.target);
-  console.log(event);
-};
-
 const RecommandPage: React.FC = () => {
+  const [ingredients, setIngredients] = useState<Array<string | null>>([]);
+  const [check, setCheck] = useState<Boolean>(false);
+  const [open, setOpen] = useState<Boolean>(false);
+
+  const history = useHistory();
+
+  const toggleCheck = (event: React.ChangeEvent<HTMLInputElement>) => {
+    let checked = event.currentTarget.checked;
+    setCheck(checked);
+    console.log(check);
+  };
+
+  const toggleIngredient = (event: MouseEvent) => {
+    event.preventDefault();
+    let target = event.currentTarget;
+    let item = target.childNodes?.[1];
+    let v = item.textContent;
+    if (ingredients.includes(v)) {
+      ingredients.splice(ingredients.indexOf(v), 1);
+      setIngredients([...ingredients]);
+    } else {
+      setIngredients([...ingredients, v]);
+    }
+    console.log(target);
+    console.log(target.classList);
+    // console.log(target);
+  };
+
+  const onSubmit = async () => {
+    const data = {
+      ingredients,
+      check,
+    };
+    const config = {
+      withCredentials: true,
+    };
+    console.log(data);
+    setOpen(!open);
+    Swal.fire({
+      text: "이 재료들로 메뉴를 추천해드릴까요?",
+      preConfirm: async () => {
+        Swal.showLoading();
+        // Swal.showValidationMessage("잠시만 기다려주세요");
+        return await axios
+          .post(RECOMMEND_LIST_URL, data, config)
+          .then((response) => {
+            if (response.status !== 200) {
+              throw new Error(response.statusText);
+            }
+            console.log(response);
+            const foodies = response.data;
+            const recommandedFood = foodies?.[0];
+            const { img_URL, recipe_ID, recipe_NM_KO } = recommandedFood;
+            Swal.fire({
+              title: "오늘의 저녁은 이거다!",
+              text: `${recipe_NM_KO}`,
+              imageUrl: `${img_URL}`,
+              showDenyButton: true,
+              confirmButtonText: "좋아요",
+              denyButtonText: "음...별로에요",
+            }).then((result) => {
+              if (result.isConfirmed) {
+                // 그리고 여기에 투표 요청 하나 넣어야지
+                history.push(`/RecoRecipe/detail/${recipe_ID}`); // 여기를 레시피 ID로 이동하도록 수정
+              }
+              if (result.isDenied) {
+                Swal.fire({
+                  title: "그럼 이건 어때요?",
+                  html:
+                    `<div style=" width:100%; height:72px; position:relative ">` +
+                    "<span style='position:absolute; top:50%; left:50%; transform:translate(-50%, -50%); color:white'>테스트dddddddddd</span>" +
+                    `<img style=" width:100%; height:100%; object-fit:cover; border-radius:15px" src=${img_URL} alt="이미지"/>` +
+                    "</div>",
+                });
+              }
+            });
+          })
+          .catch((error) => {
+            Swal.showValidationMessage(`에러가 발생했습니다.${error}`);
+          });
+      },
+      allowOutsideClick: () => !Swal.isLoading(),
+    });
+    // const res = await axios.post(RECOMMEND_LIST_URL, data, config);
+    // console.log(res);
+  };
   const classes = useStyles();
+
   return (
     <Paper className={classes.rootContainer}>
+      {open && <RecommandModal />}
       <h1>우리 집 냉장고에는??</h1>
       <Paper className={classes.container} elevation={3}>
         <section className={classes.ingredientBox}>
           <fieldset className={classes.foodFieldset}>
-            <legend
-              className={classes.foodLegends}
-              style={{ backgroundColor: "#BADA55" }}
-            >
-              메인 재료로는
-            </legend>
+            <legend className={classes.foodLegends}>메인 재료로는</legend>
             {mainIngredientMappingList.map((item) => {
               return (
                 <Chip
@@ -262,8 +313,13 @@ const RecommandPage: React.FC = () => {
                   }
                   label={item.korean}
                   className={classes.ingredientChip}
-                  onClick={testM}
-                  clickable
+                  onClick={toggleIngredient}
+                  key={uuidv4()}
+                  style={{
+                    backgroundColor: ` ${
+                      ingredients.includes(item.korean) ? "red" : "#e0e0e0"
+                    }`,
+                  }}
                 />
               );
             })}
@@ -271,12 +327,7 @@ const RecommandPage: React.FC = () => {
         </section>
         <section className={classes.ingredientBox}>
           <fieldset className={classes.foodFieldset}>
-            <legend
-              className={classes.foodLegends}
-              style={{ backgroundColor: "#BADA55" }}
-            >
-              서브 재료로는
-            </legend>
+            <legend className={classes.foodLegends}>서브 재료로는</legend>
             {subIngredientMappingList.map((item) => {
               return (
                 <Chip
@@ -284,7 +335,15 @@ const RecommandPage: React.FC = () => {
                     <Avatar alt={item.alt} src={imageObject[item.name]} />
                   }
                   label={item.korean}
+                  className={classes.ingredientChip}
+                  onClick={toggleIngredient}
                   clickable
+                  key={uuidv4()}
+                  style={{
+                    backgroundColor: ` ${
+                      ingredients.includes(item.korean) ? "red" : "#e0e0e0"
+                    }`,
+                  }}
                 />
               );
             })}
@@ -294,11 +353,11 @@ const RecommandPage: React.FC = () => {
       <h1>가 있다!!</h1>
 
       <FormControlLabel
-        control={<Checkbox name="seasoning" onChange={testF} />}
+        control={<Checkbox name="seasoning" onChange={toggleCheck} />}
         labelPlacement="start"
         label="각종 양념장도 있다!"
       />
-      <Button fullWidth variant="contained" color="primary">
+      <Button fullWidth variant="contained" color="primary" onClick={onSubmit}>
         메뉴 추천받기
       </Button>
     </Paper>
