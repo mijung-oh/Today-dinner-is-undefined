75a9539c8319dfdce158e5b9d7089fb294c52d58
SpookyJelly <74237436+SpookyJelly@users.noreply.github.com>
2021-08-17 11:27:24 +0900

    changed kakao url

--- frontend/curator/src/lib/constants.js
+++ frontend/curator/src/lib/constants.js
@@ -3,8 +3,8 @@ export const GOOGLE_URL =
 export const NAVER_URL =
   "https://nid.naver.com/oauth2.0/authorize?client_id=fUgFgzRaWuRXh8piK5n8&redirect_uri=http://127.0.0.1:3000/oauth/naver&response_type=code";
 export const KAKAO_URL =
-  "https://kauth.kakao.com/oauth/authorize?client_id=4f445ad5411d2c6c022fbd8101999e07&redirect_uri=http://127.0.0.1:3000/oauth/kakao&response_type=code";
-// "https://kauth.kakao.com/oauth/authorize?client_id=4f445ad5411d2c6c022fbd8101999e07&redirect_uri=http://I5C207.p.ssafy.io:3000/oauth/kakao&response_type=code";
+  // "https://kauth.kakao.com/oauth/authorize?client_id=4f445ad5411d2c6c022fbd8101999e07&redirect_uri=http://127.0.0.1:3000/oauth/kakao&response_type=code";
+  "https://kauth.kakao.com/oauth/authorize?client_id=4f445ad5411d2c6c022fbd8101999e07&redirect_uri=http://I5C207.p.ssafy.io:3000/oauth/kakao&response_type=code";
 
 export const USER_CHECK_URL =
   //// `http://i5c207.p.ssafy.io:9000/curation/currentLogin`, <-- 개발 끝나고 이걸로 바꿀것
